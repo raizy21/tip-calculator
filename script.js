3ffeec6609265733137eca2b5db0ceffa039f428
Razvan Andrei <chiper_raizy@yahoo.com>
2025-01-31 03:26:19 +0100

    adding tip calculator

---
+++ script.js
@@ -0,0 +1,8 @@
+function calculateTip() {
+  let bill = document.getElementById("bill").value;
+  let percentage = document.getElementById("percentage").value;
+
+  let tip = bill * (percentage / 100);
+  document.getElementById("result").innerHTML =
+    "The tip amount is:$ " + tip.toFixed(2);
+}
